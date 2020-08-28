744da68f34901ca8052a202b6d0c8908b6c37dbc
Nirmali1234 <niniperera1234@gmail.com>
2020-08-28 10:52:47 +0530

    Added Mongoose

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose //database connection string
+  .connect("mongodb://localhost/productsdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Db successfully..."))
+  .catch((err) =>
+    console.log("Error has occured while connecting to db : ", err)
+  );
+
+app.listen(PORT, function () {
+    console.log("Listening on port - " + PORT);
+  });
