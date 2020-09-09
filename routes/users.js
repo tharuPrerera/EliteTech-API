ef984a6f537b01c4eba0b09db49a4ca7bc51eba6
tharuPerera <tharuDaniyal@gmail.com>
2020-09-10 03:05:07 +0530

    fix changes

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,66 @@
 const express = require("express");
-//const user=reqire("../models/user");
-const User=require("../models/user");
+const user=reqire("../models/user");
+//const User=require("../models/user");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  let users = await User.find();
+  let users = await user.find();
   res.send(users);
 });
 
 router.post("/", async (req, res) => {
-    if (!req.body.first_name) {
-      return res.status(400).send("All fields required...!!!");
+  if(!req.body.first_name &&
+    !req.body.last_name &&
+    !req.body.email &&
+    !req.body.address &&
+    !req.body.district &&
+    !req.body.city &&
+    !req.body.postalcode &&
+    !req.body.username &&
+    !req.body.password &&
+    !req.body.confirm_password
+    ){
+      return res.status(404).send("Not all mandotry values have been set !");
     }
+
+  if (!req.body.first_name  ) {
+    return res.status(404).send("First name cannot be blank.");
+  }
+
+  if (!req.body.last_name) {
+    return res.status(404).send("Last Name cannot be blank.");
+  }
+
+  if (!req.body.email) {
+    return res.status(404).send("email cannot be blank.");
+  }
+
+  if (!req.body.address) {
+    return res.status(404).send("Address cannot be blank.");
+  }
+
+  if (!req.body.district) {
+    return res.status(404).send("district cannot be blank.");
+  }
+
+  if (!req.body.city) {
+    return res.status(404).send("City cannot be blank.");
+  }
+
+  if (!req.body.postalcode) {
+    return res.status(404).send("Postalcode cannot be blank.");
+    //return res.status(404).send("use correct num");
+  }
+
+  if (!req.body.username) {
+    return res.status(404).send("Username cannot be blank.");
+  }
+
+  if (!req.body.password) {
+    return res.status(404).send("Password cannot be blank.");
+  }
     try {
-      let userToBeAddedToDb = new User({
+      let userToBeAddedToDb = new user({
         first_name:req.body.first_name,
         last_name:req.body.last_name,
         email:req.body.email,
